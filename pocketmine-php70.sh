be415b60cf65d7c1b3631ed15b5e14ef59ab66ef
gouchaoer <765223147@qq.com>
2017-06-15 17:56:31 +0800

    fx

--- pocketmine-php70.sh
+++ pocketmine-php70.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 /pocketmine/bin/php7/bin/php -v > /dev/null 2>&1
-#如果没安装绿色版的pocketmine-php70，安装
+#如果没安装绿色版的pocketmine-php70，安装php到/pocketmine目录
 if [ $? -ne 0 ];then
-    echo "install pocketmine-php70"
-    rm -rf /pocketmine
-    sudo mkdir /pocketmine
-    wget https://coding.net/u/qsalg/p/pocketmine-php70/git/raw/master/pocketmine-php70-64.tar.gz -P /pocketmine
-    tar -zxvf /pocketmine/pocketmine-php70-64.tar.gz -C /pocketmine/
- 
+    if [ ! -f "/pocketmine/bin/php7/bin/php" ]; then  
+        echo "install pocketmine-php70"
+        sudo mkdir /pocketmine
+        wget https://coding.net/u/qsalg/p/pocketmine-php70/git/raw/master/pocketmine-php70-64.tar.gz -P /pocketmine
+        tar -zxvf /pocketmine/pocketmine-php70-64.tar.gz -C /pocketmine/
+    fi
+	#检测是否少了libltdl库
     libltdl=`/pocketmine/bin/php7/bin/php -v 2>&1 |grep "libltdl"`
     if [ -n "$libltdl" ];then
+	    echo "libltdl not found"
         lsb=`lsb_release -i |grep "CentOS"`
 		#centos可以自动安装，别的发行版手动安装
         if [ -n "$lsb" ];then
